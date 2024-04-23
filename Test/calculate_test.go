beff674937c1bca440fd7b837e3cfe0741eda500
Sukrit pipitkul <Sukrit.pi@ku.th>
2024-04-24 05:10:03 +0700

    refactor methond reponse and calculateTax

--- Test/calculate_test.go
+++ Test/calculate_test.go
@@ -127,3 +127,133 @@ func TestCalculateExp02(t *testing.T) {
 
 	require.JSONEq(t, expected, rec.Body.String())
 }
+
+func TestCalculateExp03_04(t *testing.T) {
+	// Create a new Postgres instance
+	e := echo.New()
+	reqBody := IncomeData{
+		TotalIncome: 500000.0,
+		Wht: 0,
+		Allowances: []struct {
+			AllowanceType string  `json:"allowanceType"`
+			Amount        float64 `json:"amount"`
+		}{
+			{
+				AllowanceType: "donation",
+				Amount: 200000.0,
+			},
+		},
+	}
+	reqJSON, _ := json.Marshal(reqBody)
+	req := httptest.NewRequest(http.MethodPost, "/tax/calculation", bytes.NewReader(reqJSON))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/tax/calculation")
+	p, err := postgres.New();
+	if err != nil {
+		panic(err)
+	}
+	expected := `{
+		"tax": 19000,
+		"taxLevel": [
+			{
+				"level": "0-150,000",
+				"tax": 0
+			},
+			{
+				"level": "150,001-500,000",
+				"tax": 19000
+			},
+			{
+				"level": "500,001-1,000,000",
+				"tax": 0
+			},
+			{
+				"level": "1,000,001-2,000,000",
+				"tax": 0
+			},
+			{
+				"level": "2,000,001 ขึ้นไป",
+				"tax": 0
+			}
+		]
+	}`
+	handler := calculateTax.New(p)
+	err = handler.HandleIncomeData(c)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	if rec.Code != http.StatusOK {
+		t.Errorf("Expected status 200, got %v", rec.Code)
+	}
+
+	require.JSONEq(t, expected, rec.Body.String())
+}
+
+func TestCalculateExp07(t *testing.T) {
+	// Create a new Postgres instance
+	e := echo.New()
+	reqBody := IncomeData{
+		TotalIncome: 500000.0,
+		Wht: 0,
+		Allowances: []struct {
+			AllowanceType string  `json:"allowanceType"`
+			Amount        float64 `json:"amount"`
+		}{
+			{
+				AllowanceType: "k-receipt",
+				Amount: 200000.0,
+			},
+			{
+				AllowanceType: "donation",
+				Amount: 100000.0,
+			},
+		},
+	}
+	reqJSON, _ := json.Marshal(reqBody)
+	req := httptest.NewRequest(http.MethodPost, "/tax/calculation", bytes.NewReader(reqJSON))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/tax/calculation")
+	p, err := postgres.New();
+	if err != nil {
+		panic(err)
+	}
+	expected := `{
+		"tax": 14000,
+		"taxLevel": [
+			{
+				"level": "0-150,000",
+				"tax": 0
+			},
+			{
+				"level": "150,001-500,000",
+				"tax": 14000
+			},
+			{
+				"level": "500,001-1,000,000",
+				"tax": 0
+			},
+			{
+				"level": "1,000,001-2,000,000",
+				"tax": 0
+			},
+			{
+				"level": "2,000,001 ขึ้นไป",
+				"tax": 0
+			}
+		]
+	}`
+	handler := calculateTax.New(p)
+	err = handler.HandleIncomeData(c)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	if rec.Code != http.StatusOK {
+		t.Errorf("Expected status 200, got %v", rec.Code)
+	}
+
+	require.JSONEq(t, expected, rec.Body.String())
+}
